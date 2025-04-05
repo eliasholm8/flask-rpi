28c1ee16bb0fe8350245543d7329244dc362c0e3
Elias Holm <eliholm@kth.se>
2025-04-05 23:20:59 +0200

    ip

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def run_code():
     return jsonify({"status": "Server code was executed!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
